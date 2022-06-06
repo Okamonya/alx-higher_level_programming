8eaa4477cfbbe02ba3895cccc57913bbcac747fd
Oscar Kamonya <oscarkamonya@gmail.com>
2022-06-07 00:56:51 +0300

    variables

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def find_var():
-    print(a)
-
 
 if __name__ == "__main__":
-    find_var()
+    import sys
+    sys.path.append("variable_load_5")
+    import variable_load_5 as i
+    print(i.a)
