ef15d43b97b5bde1e02b3b514e2c2b34b4ce5345
Oscar Kamonya <oscarkamonya@gmail.com>
2022-06-07 01:02:47 +0300

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-
-def arg_calc(argv):
-    n = len(argv) - 1
-    if n != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-    if op == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
 
 if __name__ == "__main__":
     import sys
-    arg_calc(sys.argv)
+    from calculator_1 import add, sub, mul, div
+
+    def getop(a, op, b):
+        if op == "+":
+            return (add(a, b))
+        if op == "-":
+            return (sub(a, b))
+        if op == "*":
+            return (mul(a, b))
+        if op == "/":
+            return (div(a, b))
+
+    nb = len(sys.argv)
+    arg = sys.argv
+    signe = "-+*/"
+    if nb == 1:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if nb == 4:
+        if arg[2] not in signe:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        r = getop(int(arg[1]), arg[2], int(arg[3]))
+        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], r))
+        exit(0)
+        
