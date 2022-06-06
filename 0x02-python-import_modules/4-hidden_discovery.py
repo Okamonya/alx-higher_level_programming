4ccf09356746d96cdcf7e020aabebef6cbedd8ea
Oscar Kamonya <oscarkamonya@gmail.com>
2022-06-07 00:53:02 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def discovr():
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
-
-
 if __name__ == "__main__":
-    discovr()
+    import hidden_4
+    for loop in sorted(dir(hidden_4)):
+        if "__" not in loop:
+            print(loop)
